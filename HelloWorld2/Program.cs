f826e1b5ab2e189dc6e7f590492d1e4d211b563b
jorgeucas lucas <jorge_dofus1@hotmail.com>
2021-03-03 16:06:41 -0300

    updates 0.1

--- HelloWorld2/Program.cs
+++ HelloWorld2/Program.cs
@@ -16,12 +16,33 @@ namespace HelloWorld2
             // != - diferente
             // >= - <=
             // int.Parse(); converte o tipo da variavel. Ex: int.Parse(Console.ReadLine());
+            // Cast: transformar o tipo de uma váriavel colocando o outro tipo entre () na frente da variavel, podendo ter perca de dados. Ex: double a = 20.16574; b = (int)a; 'b' vai se tornar 20
+
+            // + = adição
+            // - = subtração
+            // * = multiplicação
+            // / = divisão
+            // % = resto da divisão (mod)
+            // Math.Pow(x, 2.0); = x^2 (potenciação)
+            // Math.Sqrt(25); = raiz quadrada
 
             Console.WriteLine("digite 2 numeros para fazer a multiplicaçao");
             int x = int.Parse(Console.ReadLine());
             int y = int.Parse(Console.ReadLine());
-            int c = x * y;
-            Console.WriteLine("deu = " + c); ;
+            int mult = x * y;
+            Console.WriteLine("deu = " + mult); ;
+
+            Console.WriteLine("digite 2 numeros para fazer o mod da divisão");
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double resto = x2 % y2;
+            Console.WriteLine("deu = " + resto); ;
+
+            Console.WriteLine("digite 2 numeros para fazer a potenciação");
+            double x3 = double.Parse(Console.ReadLine());
+            double y3 = double.Parse(Console.ReadLine());
+            double pot = Math.Pow(x3, y3);
+            Console.WriteLine("deu = " + pot);
 
 
             Console.WriteLine("Digite sua idade ");
